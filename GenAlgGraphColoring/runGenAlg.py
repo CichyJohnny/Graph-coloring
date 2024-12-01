4315b84e1e8a75f5f75c861fb6479e170e00dc88
Jan Cichy <04cichy@gmail.com>
2024-12-01 13:27:12 +0100

    new name convention

--- GenAlgGraphColoring/runGenAlg.py
+++ GenAlgGraphColoring/runGenAlg.py
@@ -1,18 +1,21 @@
-from GraphAdjMatrix import GraphAdjMatrix
+from GraphAdjList import GraphAdjList as AdjList
+from GraphAdjMatrix import GraphAdjMatrix as AdjMatrix
 from GenAlgGC import GeneticAlgorithmGraphColoring as GenAlg
 
 if __name__ == "__main__":
     ################################################################
     # Adjustable
+    representation = AdjMatrix
+    # representation = AdjList
     filename = '../tests/test_100-0.3-1405.txt'
 
-    population_size = 200
-    mutation_rate = 0.4
+    population_size = 100
+    mutation_rate = 0.3
 
     visualize = True
     ################################################################
 
-    g = GraphAdjMatrix()
+    g = representation()
     g.load_from_file('../tests/test_100-0.3-1405.txt')
 
     gen_alg = GenAlg(g, population_size, mutation_rate, visualise=visualize)
