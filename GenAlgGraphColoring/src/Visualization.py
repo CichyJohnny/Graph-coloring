ccd47b3633deb8bdadb4507d0e7c00a27e69af63
Jan Cichy <04cichy@gmail.com>
2024-12-01 13:15:06 +0100

    added title

--- GenAlgGraphColoring/src/Visualization.py
+++ GenAlgGraphColoring/src/Visualization.py
@@ -4,11 +4,12 @@ class Visualization:
 
     # Visualize the best fitness through generations
     @staticmethod
-    def visualize(generation: int, fitness: list[int]) -> None:
+    def visualize(generation: int, fitness: list[int], numberOfColors: int) -> None:
         generations = list(range(1, generation + 1))
 
         plt.plot(generations, fitness)
 
+        plt.title(f"{numberOfColors} colors in {generation} generations")
         plt.xlabel("generation")
         plt.ylabel("best-fitness")
 
